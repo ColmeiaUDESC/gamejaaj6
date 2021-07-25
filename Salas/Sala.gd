4dc9f2898ba903732912455a0d3d1eb7fc79cbc4
Kawan Weege <47753585+DragonOfWar@users.noreply.github.com>
2021-07-25 20:29:49 -0300

    Terminar transicao de mapas (#11)
    * Terminar transicao de mapas
    
    * Ajustar padroes

--- Salas/Sala.gd
+++ Salas/Sala.gd
@@ -1,22 +1,87 @@
 extends Node2D
 
-export(NodePath) var porta_norte: NodePath
-export(NodePath) var porta_sul: NodePath
-export(NodePath) var porta_esquerda: NodePath
-export(NodePath) var porta_direita: NodePath
+const TAMANHO_SALA := Vector2(11,11)
+const DESLOCAMENTO_JOGADOR_AO_ENTRAR := 128
 
-func remover_porta(dir_porta: Vector2) -> void:
-	var caminho_porta: NodePath
-	match(dir_porta):
-		Vector2.UP:
-			caminho_porta = porta_norte
-		Vector2.DOWN:
-			caminho_porta = porta_sul
-		Vector2.LEFT:
-			caminho_porta = porta_esquerda
-		Vector2.RIGHT:
-			caminho_porta = porta_direita
-	if caminho_porta.is_empty():
-		printerr("ERRO: dir_porta de valor "+var2str(dir_porta)+" e invalido!")
+var posicao_na_matriz = Vector2.ZERO
+var dir_salas_vizinhas: Array = []
+var portas := []
 
-	get_node(caminho_porta).queue_free()
+signal entrou
+signal saiu
+
+func _ready():
+	if $Componentes/Portas.get_child_count() != 4:
+		printerr("Erro: Ha um numero incorreto de portas presentes na sala. A sala deve possuir 4 portas")
+
+	for porta in $Componentes/Portas.get_children():
+		if not dir_salas_vizinhas.has(porta.direcao):
+			porta.converter_para_parede()
+		else:
+			portas.append(porta)
+			var _erro = porta.connect("body_entered", self, "_ao_corpo_encostar_porta", [porta.direcao])
+
+	call_deferred("esconder")
+#	print(get_path(), ":ready pronto")
+
+func ao_entrar() -> void:
+	mostrar()
+	emit_signal("entrou")
+
+
+func ao_sair() -> void:
+	esconder()
+	emit_signal("saiu")
+
+
+func abrir_portas() -> void:
+	for porta in portas:
+		porta.abrir()
+
+
+func fechar_portas() -> void:
+	for porta in portas:
+		porta.fechar()
+
+
+func mostrar() -> void:
+	$Animacoes.play("aparecendo")
+
+
+func esconder() -> void:
+	$Animacoes.play("sumindo")
+
+
+func mudar_sala(dir: Vector2) -> void:
+	get_node("/root/Jogo").mudar_de_sala(get_parent().pegar_sala_em_pos(posicao_na_matriz + dir), -dir)
+
+
+func inserir_jogador(jogador: KinematicBody2D, direcao_porta: Vector2) -> void:
+	if jogador.get_parent():
+		jogador.get_parent().remove_child(jogador)
+
+	var pos_porta := pegar_pos_porta_na_direcao(direcao_porta)
+	var dir_porta_centro := pos_porta.direction_to(Vector2.ZERO)
+	jogador.position = pos_porta + dir_porta_centro * DESLOCAMENTO_JOGADOR_AO_ENTRAR
+
+	$Componentes/Jogador.call_deferred("add_child", jogador)
+
+
+func pegar_salas_vizinhas() -> Array:
+	var salas_vizinhas = []
+	for dir in dir_salas_vizinhas:
+		salas_vizinhas.append(get_parent().pegar_sala_em_pos(posicao_na_matriz + dir))
+	return salas_vizinhas
+
+
+func pegar_pos_porta_na_direcao(dir: Vector2) -> Vector2:
+	for porta in portas:
+		if porta.direcao == dir:
+			return porta.position
+
+	return position
+
+
+func _ao_corpo_encostar_porta(corpo: Node2D, dir_porta: Vector2) -> void:
+	if corpo.is_in_group("jogador"):
+		mudar_sala(dir_porta)
--- Scenes/GeradorMapa.gd
+++ Scenes/GeradorMapa.gd
@@ -4,17 +4,16 @@ export (int) var media_salas := 6
 export (int) var variancia_de_salas := 3
 export (Vector2) var tamanho_das_salas := Vector2(10, 10)
 export (Vector2) var tamanho_das_celulas := Vector2(128, 64)
-export (PackedScene) var sala_inicial: PackedScene
-export (Array, PackedScene) var salas_do_boss := []
-export (Array, PackedScene) var salas := []
+export (PackedScene) var cena_sala_inicial: PackedScene
+export (Array, PackedScene) var cena_salas_do_boss := []
+export (Array, PackedScene) var cenas_salas := []
 
+var sala_inicial: Node2D
+var sala_do_boss: Node2D
 var _salas_criadas := []
 
 const _DIRECOES := [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
 
-
-func _ready():
-	gerar()
 
 
 func gerar() -> void:
@@ -22,7 +21,8 @@ func gerar() -> void:
 	var pos_salas := [Vector2.ZERO]
 	var salas_a_criar := media_salas + rand_range(-variancia_de_salas, variancia_de_salas+1) - 1
 
-	# Gera as posicoes das salas
+	# Gera as posicoes das cenas_salas
+	var pos_mais_longe: Vector2 = pos_salas[0]
 	while (salas_a_criar > 0):
 		var sala_escolhida: Vector2 = pos_salas[rand_range(0, pos_salas.size())]
 		var pos_nova_sala: Vector2 = sala_escolhida + _dir_aleatoria()
@@ -30,34 +30,43 @@ func gerar() -> void:
 		if pos_salas.has(pos_nova_sala):
 			continue
 
+		if Vector2.ZERO.distance_squared_to(pos_nova_sala) > Vector2.ZERO.distance_squared_to(pos_mais_longe):
+			pos_mais_longe = pos_nova_sala
+
 		pos_salas.append(pos_nova_sala)
 		salas_a_criar-=1
 
-	# Cria as salas
-	var pos_mais_longe: Vector2 = pos_salas[0]
+
+	# Adiciona a posicao da sala do boss na lista de posicoes
+	var pos_sala_boss: Vector2 = Vector2.ZERO
+	for dir in _DIRECOES:
+		var pos: Vector2 =  pos_mais_longe + dir
+		if !pos_salas.has(pos) and Vector2.ZERO.distance_squared_to(pos) > Vector2.ZERO.distance_squared_to(pos_sala_boss):
+			pos_sala_boss = pos
+	pos_salas.append(pos_sala_boss)
+
+	# Cria as cenas_salas
 	for pos in pos_salas:
-		var nova_sala: PackedScene = _sala_aleatoria() if pos != Vector2.ZERO else sala_inicial
-		var sala_instancia = _criar_sala(nova_sala, pos)
+		var sala_instancia: Node2D
+
+		if pos == Vector2.ZERO:
+			sala_instancia = _criar_sala(cena_sala_inicial, pos, pos_salas)
+			sala_inicial = sala_instancia
+		elif pos == pos_sala_boss:
+			sala_instancia = _criar_sala(_sala_boss_aleatoria(), pos, pos_salas)
+			sala_do_boss = sala_instancia
+		else:
+			sala_instancia = _criar_sala(_sala_aleatoria(), pos, pos_salas)
+
 		_salas_criadas.append(sala_instancia)
 
-		if Vector2.ZERO.distance_squared_to(pos) > Vector2.ZERO.distance_squared_to(pos_mais_longe):
-			pos_mais_longe = pos
 
-		for dir in _DIRECOES:
-			if pos_salas.has(pos + dir):
-				sala_instancia.remover_porta(dir)
+func pegar_sala_em_pos(pos: Vector2):
+	for sala in get_children():
+		if sala.posicao_na_matriz == pos:
+			return sala
 
-	# Cria a sala do boss
-	var dir_sala_boss: Vector2 = Vector2.ZERO
-	for dir in _DIRECOES:
-		var pos_sala_boss: Vector2 =  pos_mais_longe + dir
-		if !pos_salas.has(pos_sala_boss) and Vector2.ZERO.distance_squared_to(pos_sala_boss) > Vector2.ZERO.distance_squared_to(pos_mais_longe + dir_sala_boss):
-			dir_sala_boss = dir
-	var sala_boss = _criar_sala(_sala_boss_aleatoria(), pos_mais_longe + dir_sala_boss)
-	sala_boss.remover_porta(-dir_sala_boss)
-	_salas_criadas[pos_salas.find(pos_mais_longe)].remover_porta(dir_sala_boss)
-	_salas_criadas.append(sala_boss)
-
+	return null
 
 
 func _converter_para_isometrico(pos: Vector2) -> Vector2:
@@ -69,15 +78,23 @@ func _dir_aleatoria() -> Vector2:
 
 
 func _sala_aleatoria() -> PackedScene:
-	return salas[randi() % salas.size()]
+	return cenas_salas[randi() % cenas_salas.size()]
 
 
 func _sala_boss_aleatoria() -> PackedScene:
-	return salas_do_boss[randi() % salas_do_boss.size()]
+	return cena_salas_do_boss[randi() % cena_salas_do_boss.size()]
 
 
-func _criar_sala(sala: PackedScene, pos: Vector2):
+func _criar_sala(sala: PackedScene, pos: Vector2, pos_salas: Array):
+	var dir_salas_vizinhas := []
+
+	for direcao in _DIRECOES:
+		if pos_salas.has(pos+direcao):
+			dir_salas_vizinhas.append(direcao)
+
 	var nova_sala = sala.instance()
 	nova_sala.position = _converter_para_isometrico(pos) * tamanho_das_salas * tamanho_das_celulas / Vector2(2,2)
+	nova_sala.posicao_na_matriz = pos
+	nova_sala.dir_salas_vizinhas = dir_salas_vizinhas
 	add_child(nova_sala)
 	return nova_sala
