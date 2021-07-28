aa942e5a6c24b0e8a19c022e75943c29618def79
DragonOfWar <therealdragonofwar@gmail.com>
2021-07-28 19:43:55 -0300

    Adicionar spawn de inimigos nas salas

--- Salas/Sala.gd
+++ Salas/Sala.gd
@@ -6,11 +6,15 @@ const DESLOCAMENTO_JOGADOR_AO_ENTRAR := 128
 var posicao_na_matriz = Vector2.ZERO
 var dir_salas_vizinhas: Array = []
 var portas := []
+var spawn_inimigos := []
+var _cont_inimigos_ativos := 0
+var _completo := false
 
 signal entrou
 signal saiu
+signal completo
 
-func _ready():
+func _ready() -> void:
 	if $Componentes/Portas.get_child_count() != 4:
 		printerr("Erro: Ha um numero incorreto de portas presentes na sala. A sala deve possuir 4 portas")
 
@@ -21,35 +25,39 @@ func _ready():
 			portas.append(porta)
 			var _erro = porta.connect("body_entered", self, "_ao_corpo_encostar_porta", [porta.direcao])
 
-	call_deferred("esconder")
+	for spawn_inimigo in $Componentes/Inimigos.get_children():
+		spawn_inimigos.append(spawn_inimigo)
+
+	if spawn_inimigos.size() == 0:
+		_completo = true
+
+	call_deferred("_esconder")
+	call_deferred("_abrir_portas")
 #	print(get_path(), ":ready pronto")
 
+
 func ao_entrar() -> void:
-	mostrar()
+	_mostrar()
+	if not _completo:
+		_fechar_portas()
+		_spawnar_inimigos()
+
 	emit_signal("entrou")
+	print(get_path(), ":entro")
 
 
 func ao_sair() -> void:
-	esconder()
+	_esconder()
+	_abrir_portas()
+	_resetar_inimigos()
 	emit_signal("saiu")
+	print(get_path(), ":saiu")
 
 
-func abrir_portas() -> void:
-	for porta in portas:
-		porta.abrir()
-
-
-func fechar_portas() -> void:
-	for porta in portas:
-		porta.fechar()
-
-
-func mostrar() -> void:
-	$Animacoes.play("aparecendo")
-
-
-func esconder() -> void:
-	$Animacoes.play("sumindo")
+func ao_completar() -> void:
+	_abrir_portas()
+	emit_signal("completo")
+	print(get_path(), ":completo")
 
 
 func mudar_sala(dir: Vector2) -> void:
@@ -82,6 +90,43 @@ func pegar_pos_porta_na_direcao(dir: Vector2) -> Vector2:
 	return position
 
 
+func _abrir_portas() -> void:
+	for porta in portas:
+		porta.abrir()
+
+
+func _fechar_portas() -> void:
+	for porta in portas:
+		porta.fechar()
+
+
+func _mostrar() -> void:
+	$Animacoes.play("aparecendo")
+
+
+func _esconder() -> void:
+	$Animacoes.play("sumindo")
+
+
+func _spawnar_inimigos() -> void:
+	for spawn_inimigo in spawn_inimigos:
+		spawn_inimigo.spawnar()
+		_cont_inimigos_ativos+=1
+
+
+func _resetar_inimigos() -> void:
+	for spawn_inimigo in spawn_inimigos:
+		spawn_inimigo.resetar()
+
+	_cont_inimigos_ativos = 0
+
+
 func _ao_corpo_encostar_porta(corpo: Node2D, dir_porta: Vector2) -> void:
 	if corpo.is_in_group("jogador"):
 		mudar_sala(dir_porta)
+
+
+func _ao_inimigo_neutralizado() -> void:
+	_cont_inimigos_ativos -= 1
+	if _cont_inimigos_ativos <= 0:
+		ao_completar()
