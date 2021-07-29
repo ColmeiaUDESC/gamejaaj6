98ee12dce7f7281f3775fe58c8628c784ff92ba1
Kawan Weege <47753585+DragonOfWar@users.noreply.github.com>
2021-07-29 13:33:45 -0300

    Fazer spawn inimigo (#21)
    * Adicionar spawn de inimigos nas salas
    
    * Finalizacoes

---
+++ ObjetosDeCenario/SpawnInimigo/SpawnInimigo.gd
@@ -0,0 +1,30 @@
+extends Position2D
+
+export(PackedScene) var cena_inimigo: PackedScene
+
+var inimigo: KinematicBody2D
+
+func spawnar() -> void:
+	if not cena_inimigo:
+		printerr("Erro: ",get_path(), " -> cena_inimigo nao foi setado")
+		return
+
+	$Delay.start()
+
+
+func resetar() -> void:
+	if inimigo:
+		inimigo.queue_free()
+
+#	print(get_path(),":reseto")
+
+
+func _on_Delay_timeout():
+	if not inimigo:
+		inimigo = cena_inimigo.instance()
+		inimigo.position = position
+		call_deferred("add_child", inimigo)
+		$Particles2D.emitting = true
+	# TODO: Fazer uma animacaozinha/Spawnar uma particula quando o inimigo spawnar
+
+#	print(get_path(),":spawno")
