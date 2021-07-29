3827409c5fd732795b57b9cfac17c9f3fca63f6a
Luciano <lucianowayand@hotmail.com>
2021-07-29 08:57:48 -0300

    implementação da barra de hp

--- Instanciaveis/Scripts/Jogador.gd
+++ Instanciaveis/Scripts/Jogador.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 export var velocidade = 250
 var movimento = Vector2()
+var vida_max = 12.0
+var vida_atual = 12.0
+var regeneracao = 0.5
 
 func _process(_delta):
 	# Everything works like you're used to in a top-down game
@@ -9,13 +12,27 @@ func _process(_delta):
 
 	if Input.is_action_pressed("ui_up"):
 		direcao += Vector2(0, -1)
+		
 	elif Input.is_action_pressed("ui_down"):
 		direcao += Vector2(0, 1)
 
 	if Input.is_action_pressed("ui_left"):
 		direcao += Vector2(-1, 0)
+		
 	elif Input.is_action_pressed("ui_right"):
 		direcao += Vector2(1, 0)
 
 	movimento = direcao.normalized() * velocidade
 	movimento = move_and_slide(movimento)
+	
+	# Debug
+	if Input.is_action_just_pressed("ui_select"):
+		vida_atual -= 1
+
+
+func _on_Regen_HP_timeout():
+	if (vida_atual + regeneracao) < vida_max:
+		vida_atual += regeneracao
+	
+	elif (vida_atual + regeneracao) >= vida_max:
+		vida_atual = vida_max
---
+++ UI/HP.gd
@@ -0,0 +1,19 @@
+extends Control
+
+onready var jogador = get_parent().get_parent()
+
+onready var aux_max = 0
+onready var aux_atual = 0
+
+func _process(_delta):
+	#So pra nao ficar mexendo no node sem necessidade
+	if aux_max != jogador.vida_max or aux_atual != jogador.vida_atual:
+		$Valores/Maximo.text = "/%d" % int(jogador.vida_max)
+		$Valores/Atual.text = str(int(jogador.vida_atual))
+		
+		# Diminui ou "esvazia" a barra relativa ao tamanho, o float e necessario pois divisao de 
+		# inteiros menor que 1 retorna zero
+		$Barra1.set("margin_right",104*(float(jogador.vida_atual)/float(jogador.vida_max)))
+		
+		aux_max = jogador.vida_max
+		aux_atual = jogador.vida_atual
