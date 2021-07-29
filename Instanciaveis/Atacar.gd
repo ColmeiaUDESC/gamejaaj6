76d2322e2e742b8564846ef16e239e6e27263858
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2021-07-29 13:51:28 -0300

    Sistema de ataque (#24)
    * ataque
    
    * implementação da barra de hp
    
    * Movimentação e limpeza de código

--- Instanciaveis/Atacar.gd
+++
@@ -1,5 +0,0 @@
-extends Estado
-
-
-func executar(_delta: float) -> void:
-	inimigo.direcao = Vector2.ZERO
--- Instanciaveis/Scripts/Jogador.gd
+++ Instanciaveis/Scripts/Jogador.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
-const VELOCIDADE = 250
+export var velocidade = 250
 var movimento = Vector2()
+var vida_max = 12.0
+var vida_atual = 12.0
+var regeneracao = 0.5
+var depois_do_ataque = false
 
 func _process(_delta):
 	# Everything works like you're used to in a top-down game
@@ -9,13 +13,42 @@ func _process(_delta):
 
 	if Input.is_action_pressed("ui_up"):
 		direcao += Vector2(0, -1)
+		
 	elif Input.is_action_pressed("ui_down"):
 		direcao += Vector2(0, 1)
 
 	if Input.is_action_pressed("ui_left"):
 		direcao += Vector2(-1, 0)
+		
 	elif Input.is_action_pressed("ui_right"):
 		direcao += Vector2(1, 0)
 
-	movimento = direcao.normalized() * VELOCIDADE
+	movimento = direcao.normalized() * velocidade
 	movimento = move_and_slide(movimento)
+	
+	# Debug
+	if Input.is_action_just_pressed("ui_select"):
+		vida_atual -= 1
+
+
+
+func inflinge_dano(dano):
+	if !depois_do_ataque:
+			vida_atual -= dano
+			intangivel() 
+
+
+func _on_Regen_HP_timeout():
+	if (vida_atual + regeneracao) < vida_max:
+		vida_atual += regeneracao
+	
+	elif (vida_atual + regeneracao) >= vida_max:
+		vida_atual = vida_max
+
+
+func intangivel():
+	depois_do_ataque = true
+	$Intangibilidade.start()
+
+func _on_Intangibilidade_timeout():
+	depois_do_ataque = false
