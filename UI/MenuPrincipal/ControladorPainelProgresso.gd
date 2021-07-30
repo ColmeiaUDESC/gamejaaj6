288e96c46cee3fba9f4b384adddec7ce01bae4c1
DragonOfWar <therealdragonofwar@gmail.com>
2021-07-29 22:27:11 -0300

    Feito menu principal e sistema de saves

---
+++ UI/MenuPrincipal/ControladorPainelProgresso.gd
@@ -0,0 +1,33 @@
+extends PanelContainer
+
+export (String) var titulo = "Nome Personagem"
+
+var puro_completo := false setget set_puro_completo, get_puro_completo
+var neutro_completo := false setget set_neutro_completo, get_neutro_completo
+var mal_completo := false setget set_mal_completo, get_mal_completo
+
+func set_puro_completo(value: bool) -> void:
+	puro_completo = value
+	$VBoxContainer/Progresso/Puro.material.set_shader_param("ativo", not value)
+
+
+func get_puro_completo() -> bool:
+	return puro_completo
+
+
+func set_neutro_completo(value: bool) -> void:
+	neutro_completo = value
+	$VBoxContainer/Progresso/Neutro.material.set_shader_param("ativo", not value)
+
+
+func get_neutro_completo() -> bool:
+	return neutro_completo
+
+
+func set_mal_completo(value: bool) -> void:
+	mal_completo = value
+	$VBoxContainer/Progresso/Mal.material.set_shader_param("ativo", not value)
+
+
+func get_mal_completo() -> bool:
+	return mal_completo
---
+++ UI/MenuPrincipal/ControladorTelaSave.gd
@@ -0,0 +1,28 @@
+extends HBoxContainer
+
+const TEXTO_CONTINUAR := "Continuar"
+const TEXTO_NOVO_JOGO := "Novo Jogo"
+
+
+func inicializar() -> void:
+	$MarginContainer/Botoes/BotaoJogar.text = TEXTO_NOVO_JOGO if DadosSave.dados.andar_atual == 0 else TEXTO_CONTINUAR
+	var progresso_personagens: Dictionary = DadosSave.dados.progresso_personagens
+	for personagem_chave in progresso_personagens.keys():
+		var puro_completo: bool = progresso_personagens[personagem_chave][0]
+		var neutro_completo: bool = progresso_personagens[personagem_chave][1]
+		var mal_completo: bool = progresso_personagens[personagem_chave][2]
+		var painel: Control
+		match (int(personagem_chave)):
+			DadosSave.Personagens.Barata:
+				painel = $Progesso/VBoxContainer/PainelProgressoBarata
+			DadosSave.Personagens.Monge:
+				painel = $Progesso/VBoxContainer/PainelProgressoMonge
+			DadosSave.Personagens.Tigre:
+				painel = $Progesso/VBoxContainer/PainelProgressoTigre
+		painel.puro_completo = puro_completo
+		painel.neutro_completo = neutro_completo
+		painel.mal_completo = mal_completo
+
+
+func _on_BotaoJogar_pressed():
+	get_tree().change_scene("res://Cenas/Jogo.tscn")
