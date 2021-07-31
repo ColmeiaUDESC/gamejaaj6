75304c51c23b60adb2160c8723125c0e9791f544
DragonOfWar <therealdragonofwar@gmail.com>
2021-07-31 19:16:27 -0300

    WIP

--- Cenas/Jogo.gd
+++ Cenas/Jogo.gd
@@ -41,8 +41,18 @@ func mudar_de_sala(nova_sala: Node2D, direcao_porta: Vector2 = Vector2.ZERO) -> 
 
 
 func _iniciar_jogo() -> void:
-	$GeradorDeMapas.gerar()
+	if DadosSave.andar_atual == 0:
+		DadosSave.andar_atual = 1
+		DadosSave.seed_atual = rand_seed(OS.get_unix_time())[0]
+		DadosSave.pureza_atual = DadosSave.PUREZA_NEUTRA
+
+	$Jogador.pureza = DadosSave.pureza_atual
+	$GeradorDeMapas.gerar(DadosSave.seed_atual)
 	call_deferred("_ir_para_sala_inicial") # Necessario chamar ir para sala inicial no proximo quadro para nao ter problema com as animacoes das portas
+
+
+func _ao_jogador_morrer() -> void:
+	DadosSave.andar_atual = 0
 
 
 func _ir_para_sala_inicial() -> void:
