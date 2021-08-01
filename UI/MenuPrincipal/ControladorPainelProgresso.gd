d193f2ca80dd156d819176a869da2ab4db5a2d2f
Kawan Weege <47753585+DragonOfWar@users.noreply.github.com>
2021-08-01 15:14:01 -0300

    Arrumado #43 (#46)

--- UI/MenuPrincipal/ControladorPainelProgresso.gd
+++ UI/MenuPrincipal/ControladorPainelProgresso.gd
@@ -6,6 +6,10 @@ var puro_completo := false setget set_puro_completo, get_puro_completo
 var neutro_completo := false setget set_neutro_completo, get_neutro_completo
 var mal_completo := false setget set_mal_completo, get_mal_completo
 
+func _ready() -> void:
+	$VBoxContainer/Titulo.text = titulo
+
+
 func set_puro_completo(value: bool) -> void:
 	puro_completo = value
 	$VBoxContainer/Progresso/Puro.material.set_shader_param("ativo", not value)
