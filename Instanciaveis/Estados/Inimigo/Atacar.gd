dae0fcd75b9c5eaa5ce5871d91cd5b31e2433382
DragonOfWar <therealdragonofwar@gmail.com>
2021-10-06 11:11:51 -0300

    Adicionado empurrao no jogador

--- Instanciaveis/Estados/Inimigo/Atacar.gd
+++ Instanciaveis/Estados/Inimigo/Atacar.gd
@@ -1,5 +1,7 @@
 extends Estado
 
+export (float) var forca_empurrao = 3000.0
+
 var esta_em_alcance = false
 var valor_ataque = 1
 
@@ -17,6 +19,7 @@ func dano():
 		if not inimigo.jogador.is_connected("recebido_dano", inimigo, "_ao_infligir_dano"):
 			inimigo.jogador.connect("recebido_dano", inimigo, "_ao_infligir_dano", [], CONNECT_ONESHOT)
 		inimigo.jogador.inflige_dano(valor_ataque)
+		inimigo.jogador.inflingir_empurrao(inimigo.global_position.direction_to(inimigo.jogador.global_position) * forca_empurrao)
 
 
 func _on_Ataque_body_entered(_body):
