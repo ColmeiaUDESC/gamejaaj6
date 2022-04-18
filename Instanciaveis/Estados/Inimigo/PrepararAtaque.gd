056c432a13ee41abab84d7210be4e11ca251f88a
Kawan Weege <therealdragonofwar@gmail.com>
2022-04-18 18:35:13 -0300

    progresso 3

--- Instanciaveis/Estados/Inimigo/PrepararAtaque.gd
+++ Instanciaveis/Estados/Inimigo/PrepararAtaque.gd
@@ -1,6 +1,5 @@
 extends Estado
 
-export var distancia_pulo := 40.0
 export var velocidade_pulo := 100.0
 export var dano := 1
 export var caminho_raycast: NodePath
@@ -10,7 +9,6 @@ onready var timer: Timer = $Timer
 
 var distancia_dano := 5.0
 var sprite: AnimatedSprite
-var progresso_pulo := distancia_pulo
 var dir_pulo = Vector2.ZERO
 var movimento := Vector2.ZERO
 var colidido := false
@@ -22,24 +20,18 @@ func ao_entrar() -> void:
 	sprite.flip_h = abs(inimigo.position.direction_to(inimigo.jogador.position).angle()) > PI*.5
 	sprite.play("carregando_ataque")
 	sprite.connect("animation_finished", self, "_ao_animacao_terminar")
-	progresso_pulo = distancia_pulo
 	inimigo.direcao = Vector2.ZERO
 	colidido = false
 
 
 func executar(delta: float) -> void:
-	if progresso_pulo < distancia_pulo:
+	if not timer.is_stopped():
 		movimento = (dir_pulo * velocidade_pulo) * delta
 		movimento = inimigo.move_and_slide(movimento)
-		progresso_pulo += movimento.length()
 		raycast.cast_to = inimigo.global_position.direction_to(inimigo.jogador.global_position) * distancia_dano
-		print(raycast.is_colliding())
 		if raycast.get_collider() == inimigo.jogador and not colidido:
-			print("sim")
 			colidido = true
 			inimigo.jogador.inflige_dano(dano)
-		if progresso_pulo >= distancia_pulo:
-			emit_signal("ataque_terminou")
 
 
 func ao_sair() -> void:
@@ -47,12 +39,17 @@ func ao_sair() -> void:
 		sprite.disconnect("animation_finished", self, "_ao_animacao_terminar")
 
 
+func _ao_timer_finalizar():
+	emit_signal("ataque_terminou")
+
+
 func _ao_animacao_terminar() -> void:
 	if sprite.animation == "carregando_ataque":
 		var jogador = inimigo.jogador
 		dir_pulo = inimigo.position.direction_to(inimigo.jogador.position)
 		sprite.play("ataque_"+inimigo._pegar_suffixo_anim_dir(dir_pulo))
-		progresso_pulo = 0.0
+		timer.start()
+		timer.connect("timeout", self, "_ao_timer_finalizar")
 	elif "ataque" in sprite.animation:
 		sprite.stop()
 		sprite.frame = sprite.frames.get_frame_count(sprite.animation) - 1
